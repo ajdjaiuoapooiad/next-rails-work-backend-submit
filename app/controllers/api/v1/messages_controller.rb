83b648772bf3eec359964952b08280fec44c198a
yukito <iyggf66974@yahoo.ne.jp>
2025-03-26 18:00:47 +0900

    erro メッセージ

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,23 +1,24 @@
 class Api::V1::MessagesController < ApplicationController
-    before_action :authorize_request
-  
-    def index
-      @messages = Message.where(sender_id: current_user.id).or(Message.where(receiver_id: current_user.id))
-      render json: @messages
+  before_action :authorize_request
+
+  def index
+    @messages = Message.where(sender_id: current_user.id).or(Message.where(receiver_id: current_user.id))
+    render json: @messages
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.sender = current_user
+    if @message.save
+      render json: @message, status: :created
+    else
+      render json: { errors: @message.errors.full_messages }, status: :unprocessable_entity
     end
-  
-    def create
-      @message = Message.new(message_params)
-      if @message.save
-        render json: @message, status: :created
-      else
-        render json: { errors: @message.errors.full_messages }, status: :unprocessable_entity
-      end
-    end
-  
-    private
-  
-    def message_params
-      params.permit(:receiver_id, :content)
-    end
+  end
+
+  private
+
+  def message_params
+    params.permit(:receiver_id, :content)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
     has_one :profile
-    has_many :messages
-    has_many :jobs, foreign_key: 'company_id' # 追加
-
+    has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id'
+    has_many :received_messages, class_name: 'Message', foreign_key: 'receiver_id'
+    has_many :jobs, foreign_key: 'company_id'
+  
     enum user_type: { student: 0, company: 1 }
-end
+  end
