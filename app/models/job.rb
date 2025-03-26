bbb74a9dd661cf4dd21569866ef4554777f06652
yukito <iyggf66974@yahoo.ne.jp>
2025-03-26 15:49:54 +0900

    jobモデル　削除　

--- app/models/job.rb
+++
@@ -1,3 +0,0 @@
-class Job < ApplicationRecord
-  belongs_to :company, class_name: 'User', foreign_key: 'company_id'
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2025_03_26_003358) do
-  create_table "jobs", force: :cascade do |t|
-    t.integer "company_id", null: false
-    t.string "title"
-    t.text "description"
-    t.integer "salary"
-    t.string "location"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["company_id"], name: "index_jobs_on_company_id"
-  end
-
   create_table "messages", force: :cascade do |t|
     t.integer "sender_id", null: false
     t.integer "receiver_id", null: false
@@ -52,7 +41,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_26_003358) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "jobs", "companies"
   add_foreign_key "messages", "receivers"
   add_foreign_key "messages", "senders"
   add_foreign_key "profiles", "users"
