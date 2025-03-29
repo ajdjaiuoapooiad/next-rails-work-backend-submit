825b478ccae17d8d4aa2f917ab22f2c63c800a95
yukito <iyggf66974@yahoo.ne.jp>
2025-03-29 17:19:27 +0900

    err

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,6 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:clean
 bundle exec rails assets:clobber
 bundle exec rails assets:precompile
 bundle exec rails db:migrate # 無料プランの場合、コメントアウトを解除
